4a8cdfc83469ce4662cb132086e0916e7c195079
Aleksandr <125996874+MrAlex-Neo@users.noreply.github.com>
2023-02-23 22:26:48 +0500

    JS. Lesson-1

---
+++ js/examples.js
@@ -0,0 +1,34 @@
+let a = Number(prompt('Первое число'))
+let b = Number(prompt('Второе число'))
+let es = prompt("Введите наименование задачи: amount (сумма чисел), arith (среднее арифметическое) или если вы хотите воспользоваться калькулятором, то введите арифметическое действие ниже (+, -, *, /)")
+let answer
+let message
+
+if (es == "amount") {
+    answer = a + b
+    message = `Ваш ответ: ${answer}!`
+} else {
+    if (es == "arith") {
+        answer = (a + b) / 2
+        message = `Ваш ответ: ${answer}!`
+    } else {
+        if (es == "+") {
+            answer = a + b
+            message = `Ваш ответ: ${answer}!`      
+        } else {
+            if (es == "-") {
+                answer = a - b
+                message = `Ваш ответ: ${answer}!`  
+            } else {
+                if (es == "*") {
+                    answer = a * b
+                    message = `Ваш ответ: ${answer}!` 
+                } else {
+                    answer = a / b
+                    message = `Ваш ответ: ${answer}!`
+                }
+            }
+        }
+    }
+}
+alert(message)
